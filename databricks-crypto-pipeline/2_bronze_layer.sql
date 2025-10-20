aa65d72167881f364f1797be339b194985282de7
Diego Bouzada <89495988+dbouzada@users.noreply.github.com>
2025-10-20 20:11:06 -0300

    Update 2_bronze_layer.sql

--- databricks-crypto-pipeline/2_bronze_layer.sql
+++ databricks-crypto-pipeline/2_bronze_layer.sql
@@ -1,14 +1,12 @@
-# Databricks notebook source
-# MAGIC %sql
-# MAGIC -- 2_bronze_layer.sql
-# MAGIC CREATE OR REPLACE TABLE crypto_db.bronze_market_data AS
-# MAGIC SELECT
-# MAGIC   id,
-# MAGIC   symbol,
-# MAGIC   name,
-# MAGIC   current_price,
-# MAGIC   market_cap,
-# MAGIC   total_volume,
-# MAGIC   current_timestamp() AS ingestion_datetime
-# MAGIC FROM crypto_db.raw_market_data;
-# MAGIC
+%sql
+-- 2_bronze_layer.sql
+CREATE OR REPLACE TABLE crypto_db.bronze_market_data AS
+SELECT
+  id,
+  symbol,
+  name,
+  current_price,
+  market_cap,
+  total_volume,
+  current_timestamp() AS ingestion_datetime
+FROM crypto_db.raw_market_data;
