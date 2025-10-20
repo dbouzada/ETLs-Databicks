52a84164152634759e605413bbaa50a100e7c8a6
Diego Bouzada <89495988+dbouzada@users.noreply.github.com>
2025-10-20 20:12:14 -0300

    Update 4_gold_layer.sql

--- databricks-crypto-pipeline/4_gold_layer.sql
+++ databricks-crypto-pipeline/4_gold_layer.sql
@@ -1,14 +1,12 @@
-# Databricks notebook source
-# MAGIC %sql
-# MAGIC -- 4_gold_layer.sql
-# MAGIC CREATE OR REPLACE TABLE crypto_db.gold_crypto_metrics AS
-# MAGIC SELECT
-# MAGIC   name,
-# MAGIC   symbol,
-# MAGIC   ROUND(price_usd, 2) AS price_usd,
-# MAGIC   ROUND(marketcap_usd / 1e9, 2) AS marketcap_billions,
-# MAGIC   ROUND(volume_usd / 1e6, 2) AS volume_millions,
-# MAGIC   ingestion_datetime
-# MAGIC FROM crypto_db.silver_market_data
-# MAGIC ORDER BY marketcap_billions DESC;
-# MAGIC
+%sql
+-- 4_gold_layer.sql
+CREATE OR REPLACE TABLE crypto_db.gold_crypto_metrics AS
+SELECT
+  name,
+  symbol,
+  ROUND(price_usd, 2) AS price_usd,
+  ROUND(marketcap_usd / 1e9, 2) AS marketcap_billions,
+  ROUND(volume_usd / 1e6, 2) AS volume_millions,
+  ingestion_datetime
+FROM crypto_db.silver_market_data
+ORDER BY marketcap_billions DESC;
