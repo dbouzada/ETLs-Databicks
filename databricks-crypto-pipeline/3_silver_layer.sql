8a6cc6c6c3630dd029f025038f61ef2c24a88f54
Diego Bouzada <89495988+dbouzada@users.noreply.github.com>
2025-10-20 20:11:42 -0300

    Update 3_silver_layer.sql

--- databricks-crypto-pipeline/3_silver_layer.sql
+++ databricks-crypto-pipeline/3_silver_layer.sql
@@ -1,16 +1,14 @@
-# Databricks notebook source
-# MAGIC %sql
-# MAGIC -- 3_silver_layer.sql
-# MAGIC CREATE OR REPLACE TABLE crypto_db.silver_market_data AS
-# MAGIC SELECT
-# MAGIC   id,
-# MAGIC   UPPER(symbol) AS symbol,
-# MAGIC   INITCAP(name) AS name,
-# MAGIC   CAST(current_price AS DOUBLE) AS price_usd,
-# MAGIC   CAST(market_cap AS DOUBLE) AS marketcap_usd,
-# MAGIC   CAST(total_volume AS DOUBLE) AS volume_usd,
-# MAGIC   ingestion_datetime
-# MAGIC FROM crypto_db.bronze_market_data
-# MAGIC WHERE current_price IS NOT NULL
-# MAGIC   AND market_cap IS NOT NULL;
-# MAGIC
+%sql
+-- 3_silver_layer.sql
+CREATE OR REPLACE TABLE crypto_db.silver_market_data AS
+SELECT
+  id,
+  UPPER(symbol) AS symbol,
+  INITCAP(name) AS name,
+  CAST(current_price AS DOUBLE) AS price_usd,
+  CAST(market_cap AS DOUBLE) AS marketcap_usd,
+  CAST(total_volume AS DOUBLE) AS volume_usd,
+  ingestion_datetime
+FROM crypto_db.bronze_market_data
+WHERE current_price IS NOT NULL
+  AND market_cap IS NOT NULL;
